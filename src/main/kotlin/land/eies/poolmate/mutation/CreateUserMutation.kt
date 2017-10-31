3ae72066d9343cd348c851998409d397d9bafa96
Erik Eiesland <erik@eies.land>
2017-10-31 20:42:23 +0100

    Fix for mutation for creating user

--- src/main/kotlin/land/eies/poolmate/mutation/CreateUserMutation.kt
+++ src/main/kotlin/land/eies/poolmate/mutation/CreateUserMutation.kt
@@ -1,21 +1,33 @@
 package land.eies.poolmate.mutation
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import graphql.schema.DataFetcher
 import graphql.schema.DataFetchingEnvironment
 import land.eies.poolmate.domain.User
 import land.eies.poolmate.graphql.GraphQLDataFetcher
 import land.eies.poolmate.graphql.GraphQLFieldBinding
+import land.eies.poolmate.repository.UserRepository
 import org.springframework.transaction.annotation.Transactional
 
 @GraphQLDataFetcher(bindings = arrayOf(
         GraphQLFieldBinding(fieldName = "createUser", parentType = "Mutation")
 ))
 @Transactional
-class CreateUserMutation : DataFetcher<User?> {
+class CreateUserMutation(
+        val objectMapper: ObjectMapper,
+        val userRepository: UserRepository
+) : DataFetcher<User?> {
 
     override fun get(environment: DataFetchingEnvironment?): User? {
         if (environment == null) {
             return null
+        }
+
+        if (environment.containsArgument("input")) {
+            val inputMap = environment.getArgument<Map<String, String>>("input").toMutableMap()
+            inputMap["hashedPassword"] = inputMap["password"]!!
+            val user = objectMapper.convertValue(inputMap, User::class.java)
+            return userRepository.save(user)
         }
 
         return null
